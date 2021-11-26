30bd3535c4af60c7dfc777221dfe6608532a1360
alexander97olsson <alexander93olsson@hotmail.com>
2021-11-26 12:10:48 +0000

    added update balance and update version

--- src/user.js
+++ src/user.js
@@ -115,6 +115,49 @@ const data = {
             await db.client.close();
         }
     },
+    updateBalance: async function updatePengar(res, req) {
+        const filter = { _id: ObjectId(req.body._id) };
+
+        if (!req.body._id ||
+            !req.body.balance) {
+            return res.status(400).json({
+                errors: {
+                    status: 400,
+                    path: "/data",
+                    title: "Bad Request",
+                    message: "Need Required parameters"
+                }
+            });
+        }
+
+        const doc = {
+            balance: parseFloat(req.body.balance),
+        };
+        let db;
+
+        try {
+            db = await database.getDb();
+            await db.userCollection.updateOne(filter, {$set: doc});
+
+            return res.status(204).json({
+                data: {
+                    result: `Object: ${req.body._id} updated`
+                }
+            });
+            
+        } catch (e) {
+            return res.status(500).json({
+                errors: {
+                    status: 500,
+                    path: "/data",
+                    title: "Database error",
+                    message: e.message
+                }
+            });
+        } finally {
+            await db.client.close();
+        }
+    },
     deleteUser: async function remove(res, req) {
         const filter = { _id: ObjectId(req.body._id) };
 
